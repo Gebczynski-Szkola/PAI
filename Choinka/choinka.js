14c104455f9cb74c401a3b44a55ea3d14937ca9d
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2020-12-16 12:47:25 +0100

    połowa drzewa

--- Choinka/choinka.js
+++ Choinka/choinka.js
@@ -1,10 +1,25 @@
-const pojemnik = document.querySelector("#p");
+const pojemnik = document.querySelector("#output");
 //console.log(pojemnik);
+const wysokosc = document.querySelector("#wysokosc");
+const button = document.querySelector("#send");
 
-let wielkosc = 5;
+button.addEventListener("click", wypiszChoinke);
+
+let wielkosc = wysokosc.value;
 
 function wypiszChoinke(wielkosc) {
-    for (let i = 1; i <= wielkosc; i++) {
+    const level = parseInt(wysokosc.value);
+    let tree = "";
 
+    for (let i = 1; i <= level + 1; i++) {//wysokosc choinki 
+        for (let j = 1; j < i; j++) {//wypisanie gwaizdek 
+            tree += "*";
+        }
+        for (let k = 1; k <= 1; k++) {
+            tree += "&nbsp;"
+        }
+        tree += "<br>";
     }
+
+    pojemnik.innerHTML = tree;
 }
