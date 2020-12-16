ac1a8f80c2b7c1b64713c6e861f01528c09c6a13
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2020-12-16 12:12:31 +0100

    główna pętla

---
+++ Choinka/choinka.js
@@ -0,0 +1,10 @@
+const pojemnik = document.querySelector("#p");
+//console.log(pojemnik);
+
+let wielkosc = 5;
+
+function wypiszChoinke(wielkosc) {
+    for (let i = 1; i <= wielkosc; i++) {
+
+    }
+}
