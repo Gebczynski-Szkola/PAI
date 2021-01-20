03ee40660996a2f15948d167bcfcfda98617a477
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2021-01-20 21:56:30 +0100

    index.html and css

---
+++ nameColor/script/script.js
@@ -0,0 +1,4 @@
+let name = document.querySelector("#name");
+let age = document.querySelector("#age");
+let color = document.querySelector("#color");
+const submit = document.querySelector("#submit")
